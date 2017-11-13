4ff6f03b7e9e3e01acbcd5c6be3fdfc379ebaa18
Maki <Maki@MAKI97-PC>
2017-11-13 06:21:28 +0100

    Novi projekat

---
+++ Projekat-sf12-gui/POP-SF-12-GUI/UI/NamstajWindowDodavanjeIzmena.xaml.cs
@@ -0,0 +1,79 @@
+﻿using POP_SF_9_GUI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace POP_SF_9_GUI.UI
+{
+    /// <summary>
+    /// Interaction logic for NamstajWindowDodavanjeIzmena.xaml
+    /// </summary>
+    
+    public partial class NamstajWindowDodavanjeIzmena : Window
+    {
+        private Namestaj namestaj;
+        private Operacija operacija;
+        public enum Operacija
+        {
+            DODAVANJE,
+            IZMENA
+        };
+        public NamstajWindowDodavanjeIzmena(Namestaj noviNamestaj, Operacija operacija)
+        {
+            InitializeComponent();
+
+            InicijalizujVrednosti(namestaj, operacija);
+        }
+        private void InicijalizujVrednosti(Namestaj namestaj, Operacija operacija)
+        {
+            this.namestaj = namestaj;
+            this.operacija = operacija;
+
+            tbNaziv.Text = namestaj.Naziv;
+
+        }
+        private void Izlaz(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void SacuvajNamestaj(object sender, RoutedEventArgs e)
+        {
+            List<Namestaj> postojeciNamestaj = Projekat.Instance.Namestaj;
+            switch (operacija)
+            {
+
+                case Operacija.DODAVANJE:
+                    var noviNamestaj = new Namestaj()
+                    {
+                        Naziv = tbNaziv.Text
+                    };
+                    postojeciNamestaj.Add(noviNamestaj);
+                    break;
+                case Operacija.IZMENA:
+                    foreach (var n in postojeciNamestaj)
+                    {
+                        if (n.Id == namestaj.Id)
+                        {
+                            n.Naziv = tbNaziv.Text;
+                            break;
+                        }
+                    }
+                    break;
+
+
+            }
+            Projekat.Instance.Namestaj = postojeciNamestaj;
+            this.Close();
+        }
+    }
+}
