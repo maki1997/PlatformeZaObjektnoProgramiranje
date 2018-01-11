71cc49960e3ef012fa76215f45a3ca1a44d647b5
maki1997 <makibijelic3377@gmail.com>
2018-01-11 20:04:57 +0100

    Iz nekog razlog prethodni push nije prosao

--- NOVI PROJEKAT SF 12 GUI/SF-12-2016/SF-12-2016/GUI/EditTipa.xaml.cs
+++ NOVI PROJEKAT SF 12 GUI/SF-12-2016/SF-12-2016/GUI/EditTipa.xaml.cs
@@ -1,4 +1,4 @@
-﻿using SF_12_2016.Model;
+using SF_12_2016.Model;
 using SF_12_2016.Utils;
 using System;
 using System.Collections.Generic;
@@ -44,17 +44,17 @@ namespace SF_12_2016.GUI
         private void btSacuvaj_Click(object sender, RoutedEventArgs e)
         {
             var postojeciTNamestaj = Projekat.Instance.tipovi;
+
             if (tbNaziv.Text.Trim() == string.Empty)
             {
                 MessageBox.Show("Niste uneli Naziv");
                 return; // return because we don't want to run normal code of buton click
             }
+
             switch (operacija)
             {
                 case Operacija.DODAVANJE:
-                    var Id = postojeciTNamestaj.Count + 1;
-                    tipNamestaja.Id = Id;
-                    postojeciTNamestaj.Add(tipNamestaja);
+                    TipNamestaja.Create(tipNamestaja);
                     break;
                 case Operacija.IZMENA:
                     foreach (var n in postojeciTNamestaj)
@@ -62,48 +62,17 @@ namespace SF_12_2016.GUI
                         if (n.Id == tipNamestaja.Id)
                         {
                             n.Naziv = tipNamestaja.Naziv;
-
+                            TipNamestaja.Update(n);
                             break;
                         }
                     }
                     break;
             }
-            GenericSerializer.Serialize("tipnamestaja.xml", postojeciTNamestaj);
+
 
             this.Close();
 
         }
 
-        private void Button_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                var postojeciTNamestaj = Projekat.Instance.tipovi;
-
-                switch (operacija)
-                {
-                    case Operacija.DODAVANJE:
-                        var Id = postojeciTNamestaj.Count + 1;
-                        tipNamestaja.Id = Id;
-                        postojeciTNamestaj.Add(tipNamestaja);
-                        break;
-                    case Operacija.IZMENA:
-                        foreach (var n in postojeciTNamestaj)
-                        {
-                            if (n.Id == tipNamestaja.Id)
-                            {
-                                n.Naziv = tipNamestaja.Naziv;
-
-                                break;
-                            }
-                        }
-                        break;
-                }
-                GenericSerializer.Serialize("tipnamestaja.xml", postojeciTNamestaj);
-
-                this.Close();
-                }
-            }
-
-        }
+    }
 }
